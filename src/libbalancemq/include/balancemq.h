6284b434cf436d321107e97e6ac8f2191e4de8a7
jonb <jonb@lanl.gov>
2013-03-19 13:49:38 -0600

    Addition of a common header.

---
+++ src/libbalancemq/include/balancemq.h
@@ -0,0 +1,13 @@
+/**
+ * @file
+ *
+ * This is a header to simplify including all BalanceMQ headers.
+ */
+
+#ifndef _BALANCEMQ_BALANCEMQ_H
+#define _BALANCEMQ_BALANCEMQ_H
+
+#include <balancemq/settings.h>
+#include <balancemq/api.h>
+
+#endif /* _BALANCEMQ_BALANCEMQ_H */
